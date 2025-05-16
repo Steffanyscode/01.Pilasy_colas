b7dd4b08e30f7da19fed16b49a9c59a5c393ac70
Fannya G <steffanyscode30@gmail.com>
2025-05-15 19:30:46 -0600

    ✨ archivos no deseados en github

---
+++ 0.4.CRUD/app.js
@@ -0,0 +1,5 @@
+//URL DE LA API REST DONDE SE ALMACENAN LOS PRODUCTOS
+const API_URL = 'https:localhost:3000/products'
+
+//Referencia a los elemnetos del dom que interactuan con el usuario
+const productForm = documento
